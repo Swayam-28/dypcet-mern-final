60b9e0737d03bda3f2c83c2760ecd842f90d8c39
Swayam-28 <your-email@example.com>
2025-11-09 19:53:57 +0530

    fix: removed duplicate route imports and ready for deploy

--- server/src/index.js
+++ server/src/index.js
@@ -13,19 +13,19 @@ import adm from './routes/admissionRoutes.js'
 import plc from './routes/placementRoutes.js'
 import fac from './routes/facultyRoutes.js'
 import noti from './routes/noticeRoutes.js'
-// import gal from './routes/galleryRoutes.js'
 import upload from './routes/uploadRoutes.js'
-import enquiry from './routes/enquiryRoutes.js';
-import facultyRoutes from "./routes/facultyRoutes.js";
-import newsRoutes from "./routes/newsRoutes.js";
+import enquiry from './routes/enquiryRoutes.js'
 import galleryRoutes from "./routes/galleryRoutes.js";
-import enquiryRoutes from "./routes/enquiryRoutes.js";
-
-
 
 dotenv.config()
+
 const app = express()
-app.use(cors({ origin: process.env.ORIGIN?.split(',') || '*', credentials: true }))
+
+app.use(cors({
+  origin: process.env.ORIGIN?.split(',') || '*',
+  credentials: true
+}))
+
 app.use(express.json({ limit:'5mb' }))
 app.use(morgan('dev'))
 
@@ -43,14 +43,10 @@ app.use('/api/admissions', adm)
 app.use('/api/placements', plc)
 app.use('/api/faculty', fac)
 app.use('/api/notices', noti)
-// app.use('/api/gallery', gal)
 app.use('/api/upload', upload)
-app.use('/api/enquiry', enquiry);
-app.use('/api/faculty', facultyRoutes);
-app.use('/api/news', newsRoutes);
+app.use('/api/enquiry', enquiry)
 app.use("/api/galleryitems", galleryRoutes);
-app.use("/api/enquiries", enquiryRoutes);
 
 const port = process.env.PORT || 5000
-const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/dypcet_clone'
-connectDB(uri).then(()=> app.listen(port, ()=> console.log('API running :'+port)))
+const uri = process.env.MONGO_URI
+connectDB(uri).then(()=> app.listen(port, ()=> console.log('API running :' + port)))
